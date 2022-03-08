357dc81fc39380d719c7023f8b48ce26ed5823fd
Skoro-Dani <skorodaniel@gmail.com>
2022-03-08 17:16:26 +0100

    Esercizio

--- BotPublicita/src/botpublicita/BotPublicita.java
+++ BotPublicita/src/botpublicita/BotPublicita.java
@@ -6,20 +6,28 @@
 package botpublicita;
 
 import TelegramApi.*;
+import java.io.IOException;
+import java.util.List;
 
 /**
  *
  * @author skorolitniy_daniel
  */
 public class BotPublicita {
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        Test ciao=new Test();
-        ciao.foo();
+        String Token = "5289178489:AAGNqdWsbQcFq5Wbg4agZ9AQjr8bGTBQ_NA";
+        JSONfile json=new JSONfile();
+        json.CreaFile("GetUpdates.txt",Token);
+        List<String> id = json.getChatID("GetUpdates.txt");
+        for(int i=0;i<id.size();i++)
+        {
+            json.SendMessage(Token, id.get(i), "scherzo ti voglio bene");
+        }
+        
     }
     
 }
